cc7f0d4bb6e8f13735c548ee25b17101b29cbcb1
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-05-03 15:49:41 +0800

    admin login

--- src/controller/AdminAuthController.ts
+++ src/controller/AdminAuthController.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import { Request, Response, NextFunction } from 'express';
 import { AppDataSource } from '../data-source';
-import { User } from '../entity/User';
+
 import {Admins} from '../entity/Admin'
 import jwt from 'jsonwebtoken';
 
@@ -15,7 +15,7 @@ interface LoginResponseBody {
   token?: string;
 }
 
-export const login = async (
+export const adminlogin = async (
   req: Request<{}, {}, LoginRequestBody>,
   res: Response<LoginResponseBody>,
   next: NextFunction
@@ -23,7 +23,7 @@ export const login = async (
   try {
     const { email, password } = req.body;
 
-    const userRepository = AppDataSource.getRepository(User);
+    const userRepository = AppDataSource.getRepository(Admins);
 
     const user = await userRepository.findOne({ where: { email } });
     if (!user) {
@@ -53,37 +53,3 @@ export const login = async (
 
 
 
-
-
-// // Example without bcrypt: direct comparison (plain text comparison)
-// export const login = async (
-//   req: Request<{}, {}, LoginRequestBody>,
-//   res: Response<LoginResponseBody>,
-//   next: NextFunction
-// ): Promise<void> => {
-//   try {
-//     const { email, password } = req.body;
-
-//     const userRepository = AppDataSource.getRepository(User);
-
-//     const user = await userRepository.findOne({ where: { email } });
-//     if (!user) {
-//       res.status(401).json({ message: "Invalid credentials" });
-//       return;
-//     }
-
-//     // Directly compare plain text password (not secure, for testing only)
-//     if (password !== user.password) {
-//       res.status(401).json({ message: "Invalid credentials" });
-//       return;
-//     }
-
-//     const token = jwt.sign({ id: user.id, email: user.email }, "your-secret-key", {
-//       expiresIn: "1h",
-//     });
-
-//     res.json({ message: "Login successful", token });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { authenticate, AuthenticatedRequest, } from './../middleware/authmiddleware';
 import express, {Response} from 'express';
 import { login } from '../controller/AuthController' // Import the login function
+import {adminlogin} from '../controller/AdminAuthController'
 
 const router = express.Router();
 
 router.post('/userlogin', login); // Attach the login controller to the route
 
-
+router.post('/adminlogin', adminlogin)
 
 // Login
 router.post("/login", login);
